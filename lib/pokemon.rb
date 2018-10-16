d0d0b230c667f534d2bbafe4ef56c3f8dab93cd3
bluebeadlily <bluebeadlily@gmail.com>
2018-10-16 02:04:08 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,7 +12,7 @@ class Pokemon
   end
   
   def self.save(name, type, database_connection)
-    database_connection.execute("INSERT INTO cats (name, breed, age) VALUES (?, ?, ?)",name, breed, age)
+    database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
   end
  
 end
