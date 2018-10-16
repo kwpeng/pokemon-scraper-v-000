85304dc077472db387134b32abeb5f8fed4cc5fc
bluebeadlily <bluebeadlily@gmail.com>
2018-10-16 01:57:21 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,4 @@
 class Pokemon
 end
+
+#For our purposes the Pokemon class is responsible for saving, adding, removing, or changing anything about each Pokémon. Your scraper is not responsible for knowing anything about them.
