fe5c9b94e64351f0f00e1f39db2f127c0493e020
bluebeadlily <bluebeadlily@gmail.com>
2018-10-16 02:03:26 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,8 +1,20 @@
 class Pokemon
   @@all = []
  
-  def initialize(name, breed, age)
+  def initialize(name, type)
+    @name = nanme
+    @type = type
+    @@all << self
+  end
   
+   def self.all
+    @@all
+  end
+  
+  def self.save(name, type, database_connection)
+    database_connection.execute("INSERT INTO cats (name, breed, age) VALUES (?, ?, ?)",name, breed, age)
+  end
+ 
 end
 
 #For our purposes the Pokemon class is responsible for saving, adding, removing, or changing anything about each Pokémon. Your scraper is not responsible for knowing anything about them.
